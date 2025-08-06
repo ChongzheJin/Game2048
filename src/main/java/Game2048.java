da2b8c9484e7ca59f33b7f5d345aa192f4dbb22e
Chongzhe Jin <johnnyjinchongzhe@gmail.com>
2025-08-06 23:49:44 +0800

    update main method

--- src/main/java/Game2048.java
+++ src/main/java/Game2048.java
@@ -8,16 +8,36 @@ import view.GameView;
 import java.util.Scanner;
 
 /**
+ * This is the main class of the game.
+ *
  * @author Chongzhe Jin
  * @version 1.0, 2025/8/5
  * @since 2025/8/5 00:24
  */
 public class Game2048 {
     public static void main(String[] args) {
-        GameModel model = new GameModelImpl();
-        GameView view = new ConsoleGameView(new Scanner(System.in));
-        GameController controller = new ConsoleGameController(model, view);
+        if (args.length == 0) {
+            System.err.println("Usage: java Game2048 <mode>");
+            System.err.println("Mode: console, gui");
+            System.exit(1);
+        }
+        String mode = args[0].toLowerCase().trim();
 
-        controller.start();
+        switch (mode) {
+            case "console":
+                GameModel model = new GameModelImpl();
+                GameView view = new ConsoleGameView(new Scanner(System.in));
+                GameController controller = new ConsoleGameController(model, view);
+
+                controller.start();
+                break;
+            case "gui":
+                // TODO: implement GUI
+                break;
+            default:
+                System.err.println("Unknown mode: " + mode);
+                System.err.println("Mode: console, gui");
+                System.exit(1);
+        }
     }
 }
